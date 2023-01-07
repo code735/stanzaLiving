0f8cfed7f36c6bc7d743ff28926f700a79be069b
Prathmesh Kumbhar <prathmeshjkumbhar@gmail.com>
2023-01-07 20:44:26 +0530

    readme file

--- individualPage/individualPage.js
+++ individualPage/individualPage.js
@@ -1,4 +1,4 @@
-import { navComponent } from "../components/searchbox.js";
+import { navComponent, footerComponent } from "../components/searchbox.js";
 
 // Localstorage
 
@@ -12,6 +12,8 @@ document.title = individualData.product.seoTitle;
 // Navbar appending
 document.querySelector("header").innerHTML = navComponent;
 
+// footer appending
+document.getElementById("footer_holder").innerHTML = footerComponent;
 
 let ex_recidence_btn = document.getElementById("exlpore_residence_link");
 
